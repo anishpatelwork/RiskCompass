6d6242d9a05276315d00e665210eb43a50d93eb5
anishpatelwork <anishpatelwork@gmail.com>
2018-04-11 15:42:03 +0100

    Added a radar chart to the results page. Need to drive it using the results of the RMB

--- compass/views.py
+++ compass/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
 from compass.models import RMB
+from pyecharts import Radar
+import math
 
 # Create your views here.
 
@@ -30,7 +34,28 @@ def answer(request, rmb_id, question_id, answer_id):
         return redirect(f'/rmb/{rmb_id}/question/{next_question_id}')
 
 
-def results(request, rmb_id):
-    rmb = RMB.objects.get(id=rmb_id)
+REMOTE_HOST = "https://pyecharts.github.io/assets/js"
 
-    return render(request, 'results.html', {'answer_list': rmb.answer_list})
+
+def results(request, rmb_id):
+    template = loader.get_template('results.html')
+    rmb = RMB.objects.get(id=rmb_id)
+    l3d = radar()
+    context = dict(
+        myechart=l3d.render_embed(),
+        host=REMOTE_HOST,
+        script_list=l3d.get_js_dependencies()
+    )
+    return HttpResponse(template.render(context, request))
+
+
+def radar():
+    schema = [("销售", 6500), ("管理", 16000), ("信息技术", 30000), ("客服", 38000), ("研发", 52000), ("市场", 25000)]
+    radar = Radar()
+    v1 = [[4300, 10000, 28000, 35000, 50000, 19000]]
+    v2 = [[5000, 14000, 28000, 31000, 42000, 21000]]
+    radar.config(schema)
+    radar.add("预算分配", v1, is_splitline=True, is_axisline_show=True)
+    radar.add("实际开销", v2, label_color=["#4e79a7"], is_area_show=False)
+    radar.show_config()
+    return radar

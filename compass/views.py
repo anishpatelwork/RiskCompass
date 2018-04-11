edc1081af453f77bbd2f893ff0e242f395850f44
anishpatelwork <anishpatelwork@gmail.com>
2018-04-11 17:22:53 +0100

    tidying up results

--- compass/views.py
+++ compass/views.py
@@ -50,12 +50,9 @@ def results(request, rmb_id):
 
 
 def radar():
-    schema = [("销售", 6500), ("管理", 16000), ("信息技术", 30000), ("客服", 38000), ("研发", 52000), ("市场", 25000)]
+    schema = [("Data Quality", 5), ("Cat Modeling", 5), ("Non Modeled", 5), ("Profiling Submissions", 5), ("Pricing", 5), ("Binding", 5), ("Capacity Management", 5), ("Risk Transfer", 5), ("Event Response", 5), ("Reporting", 5)]
     radar = Radar()
-    v1 = [[4300, 10000, 28000, 35000, 50000, 19000]]
-    v2 = [[5000, 14000, 28000, 31000, 42000, 21000]]
-    radar.config(schema)
-    radar.add("预算分配", v1, is_splitline=True, is_axisline_show=True)
-    radar.add("实际开销", v2, label_color=["#4e79a7"], is_area_show=False)
-    radar.show_config()
+    v1 = [[3, 2, 2, 5, 3, 5, 3, 5, 1, 4]]
+    radar.config(schema, shape="polygon")
+    radar.add("Results", v1, is_legend_show=False, is_label_show=False, is_toolbox_show=False)
     return radar

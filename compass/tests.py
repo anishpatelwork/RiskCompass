4286740ec20da6d48ff23167a76bdd2c11fb4a56
anishpatelwork <anishpatelwork@gmail.com>
2018-04-11 11:21:22 +0100

    Adding home page template and assigning it to home

--- compass/tests.py
+++ compass/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.template.loader import render_to_string
+from django.http import HttpRequest
 from compass.views import home_page
 
 # Create your tests here.
@@ -10,3 +12,7 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
